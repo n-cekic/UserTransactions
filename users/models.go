2690e09b4ee10d6e003778ad42a5f4b91a0dc9b8
Nikola Cekic <ncekic13@gmail.com>
2024-02-16 14:31:06 +0100

    remove username

--- users/models.go
+++ users/models.go
@@ -1,11 +1,10 @@
 package users
 
 type createUserRequest struct {
-	Usernme string `json:"username"`
-	Email   string `json:"email"`
+	Email string `json:"email"`
 }
 
 type createUserResponse struct {
-	Usernme string `json:"username"`
-	Email   string `json:"email"`
+	Email string `json:"email"`
+}
 }
