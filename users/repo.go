cb3ac6c1d11c8a0cb81d8fa4ce19e21d1d1dfe78
Nikola Cekic <ncekic13@gmail.com>
2024-02-16 12:24:31 +0100

    create user insert to DB implemented

--- users/repo.go
+++ users/repo.go
@@ -1,3 +1,22 @@
 package users
 
-type Repo struct{}
+import (
+	"database/sql"
+	L "userTransactions/logging"
+
+	_ "github.com/lib/pq"
+)
+
+type Repo struct {
+	db *sql.DB
+}
+
+func (r *Repo) createUser(email string) error {
+	q := `INSERT INTO "user" ("email") VALUES ($1)`
+
+	_, err := r.db.Exec(q, email)
+	if err != nil {
+		L.Logger.Printf("New user insert failed: %s", err.Error())
+	}
+	return err
+}
