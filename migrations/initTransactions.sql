ef1df4e5a634acc2765fde10343aa6efc27477c9
Nikola Cekic <ncekic13@gmail.com>
2024-02-16 17:58:53 +0100

    create table transaction

--- migrations/initTransactions.sql
+++ migrations/initTransactions.sql
@@ -4,9 +4,18 @@ CREATE DATABASE transactions;
 -- Connect to the newly created database
 \c transactions;
 
--- Crea the 'user_balance' table
-CREATE TABLE "user_balance" (
+-- Crea the 'userbalance' table
+CREATE TABLE "userbalance" (
     user_id SERIAL PRIMARY KEY,
     balance VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Crea the 'transaction' table
+CREATE TABLE "transaction" (
+    id SERIAL PRIMARY KEY,
+    "type" VARCHAR(10) NOT NULL,
+    "from" VARCHAR(255) NOT NULL,
+    "to" VARCHAR(255),
+    "timestamp" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
