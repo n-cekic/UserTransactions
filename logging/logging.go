c348a570a026c973c7e6883a4cf03c7decf373c6
Nikola Cekic <ncekic13@gmail.com>
2024-02-16 12:23:53 +0100

    refactor logger

--- logging/logging.go
+++ logging/logging.go
@@ -2,8 +2,30 @@ package logging
 
 import "log"
 
-var Logger *log.Logger
+// var Logger *log.Logger
+
+type Log struct {
+	logger *log.Logger
+}
+
+var Logger Log
 
 func init() {
-	Logger = log.Default()
+	Logger.logger = log.Default()
+}
+
+func (l *Log) Printf(s string, v ...any) {
+	l.logger.Printf(s, v...)
+}
+
+func (l *Log) Print(v ...any) {
+	l.logger.Print(v...)
+}
+
+func (l *Log) Println(v ...any) {
+	l.logger.Println(v...)
+}
+
+func (l *Log) Fatalf(s string, v ...any) {
+	l.logger.Printf(s, v...)
 }
