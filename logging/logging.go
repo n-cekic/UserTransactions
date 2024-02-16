1d280965af924319c559c0e5828423ac4ae33a8e
Nikola Cekic <ncekic13@gmail.com>
2024-02-16 17:37:42 +0100

    logging refactoring

--- logging/logging.go
+++ logging/logging.go
@@ -16,25 +16,25 @@ func init() {
 }
 
 func (l *Log) Info(v ...any) {
-	l.logger.Print("[INFO]" + fmt.Sprint(v...))
+	l.logger.Print("[INFO]\t" + fmt.Sprint(v...))
 }
 
 func (l *Log) Infof(s string, v ...any) {
-	l.logger.Printf("INFO" + fmt.Sprintf(s, v...))
+	l.logger.Printf("[INFO]\t" + fmt.Sprintf(s, v...))
 }
 
 func (l *Log) Error(v ...any) {
-	l.logger.Print("[ERROR]" + fmt.Sprint(v...))
+	l.logger.Print("[ERROR]\t" + fmt.Sprint(v...))
 }
 
 func (l *Log) Errorf(s string, v ...any) {
-	l.logger.Printf("ERROR" + fmt.Sprintf(s, v...))
+	l.logger.Printf("[ERROR]\t" + fmt.Sprintf(s, v...))
 }
 
 func (l *Log) Fatal(v ...any) {
-	l.logger.Fatal("[FATAL]" + fmt.Sprint(v...))
+	l.logger.Fatal("[FATAL]\t" + fmt.Sprint(v...))
 }
 
 func (l *Log) Fatalf(s string, v ...any) {
-	l.logger.Fatalf("FATAL" + fmt.Sprintf(s, v...))
+	l.logger.Fatalf("[FATAL]\t" + fmt.Sprintf(s, v...))
 }
